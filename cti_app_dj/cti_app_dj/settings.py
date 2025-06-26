924fe4cf0f1606a50b35bae78b257fa8478a8150
drbynz0 <infosastuce78@gmail.com>
2025-06-26 22:43:20 +0100

    Refactor settings to use environment variables and add var.env for configuration

--- cti_app_dj/cti_app_dj/settings.py
+++ cti_app_dj/cti_app_dj/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Charge les variables d'environnement
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,11 +35,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nizarsoilih@gmail.com'
-EMAIL_HOST_PASSWORD = 'gnyabzbxdobcultd'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-GROQ_API_KEY = "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"  # À mettre dans les variables d'environnement
-GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+GROQ_API_URL = os.getenv('GROQ_API_URL')
 
 # Application definition
 
@@ -134,12 +140,12 @@ AUTH_USER_MODEL = 'users.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cti_app_db',
-        'USER': 'postgres',
-        'PASSWORD': 'drbynz00',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
